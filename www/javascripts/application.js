9783bc8ddedab4c16ce16740a197e8aa06f9b300
Joe Formica <joe.v.formica@gmail.com>
2014-02-09 22:52:43 -0500

    Facebook login

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -1,60 +1,71 @@
-//Fade in subtitle on homepage
-$(document).ready(function(){
-    $(".subtitle").hide(0).delay(500).fadeIn(3000)
-});
 
-  // Create Hub webview/push to signup screen
-  var hubView = new steroids.views.WebView("../hub.html");
-  hubView.preload();
-  
-  function openShow() {
+var hubView = new steroids.views.WebView("../hub.html");
+hubView.preload();
+
+var homeView = new steroids.views.WebView("../index.html");
+homeView.preload();
+
+function hubPush() {
     steroids.layers.push(hubView);
-  }
+};
+
+function homePush() {
+    steroids.layers.push(homeView);
+};
 
 
-  window.fbAsyncInit = function() {
-// init the Facebook JavaScript SDK
+
+window.fbAsyncInit = function () {
+    // init the Facebook JavaScript SDK
     FB.init({
-        appId       : '213402672198380',
-        channelUrl  : "localhost/channel.html",
-        status      : false,
-        cookie      : true,
-        xfbml       : true
+        appId: '213402672198380',
+        channelUrl: "localhost/channel.php",
+        status: true,
+        cookie: true,
+        xfbml: true,
+        oauth: true
     });
 
-    FB.getLoginStatus(function(response) {
+    FB.getLoginStatus(function (response) {
         if (response.status === 'connected') {
-          
+            // window.location.replace("http://localhost/hub.html");
         } else if (response.status === 'not_authorized') {
-            // not authorized
-            
+            alert("youre not authorized");
+
         } else {
-            // not_logged_in
-            
+            function sendHome() {
+                window.location.replace("http://localhost/index.html");
+            }
         }
     });
 };
 
 function login() {
-    FB.login(function(response) {
+    FB.login(function (response) {
         if (response.authResponse) {
-            // connected
-            mainpush();
+            FB.api('/me', function (response) {
+                window.location.replace("http://localhost/hub.html");
+                // FB.logout(function (response) {
+                //     console.log('Logged out.');
+                // });
+            });
         } else {
-            // cancelled
+            console.log('User cancelled login or did not fully authorize.');
         }
+    }, {
+        scope: 'email,publish_stream',
+        display: 'touch'
     });
 }
 
-function mainpush() {
-    steroids.layers.push(hubView);
-    };
+
 
 function logout() {
-    FB.logout(function(response) {
+    FB.logout(function (response) {
         if (response.authResponse) {
             // connected
-            alert("person is jogged out");
+             alert("person is logged out");
+
         } else {
             // cancelled
         }
@@ -63,10 +74,17 @@ function logout() {
 
 
 
-(function(d){
-    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement('script'); js.id = id; js.async = true;
+(function (d) {
+    var js, id = 'facebook-jssdk',
+        ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
     js.src = "//connect.facebook.net/en_US/all.js";
     ref.parentNode.insertBefore(js, ref);
 }(document));
+
+
